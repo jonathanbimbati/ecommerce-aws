2b2d2e92d29d231d05efaf896634ac15e00d2d69
jbimbati <jonathanbimbati@gmail.com>
2025-10-14 00:42:27 -0300

    chore: ignore build caches (.angular, .aws-sam, frontend/dist)

--- .aws-sam/build/ApiFunction/lambda/apiHandler.js
+++
@@ -1,6 +0,0 @@
-const serverlessExpress = require('@vendia/serverless-express');
-const app = require('../index');
-
-const server = serverlessExpress({ app });
-
-exports.handler = (event, context) => server(event, context);
--- .aws-sam/build/NotificationConsumerFunction/lambda/apiHandler.js
+++
@@ -1,6 +0,0 @@
-import serverlessExpress from '@vendia/serverless-express';
-import app from '../index';
-
-const server = serverlessExpress({ app });
-
-export function handler(event, context) { return server(event, context); }
